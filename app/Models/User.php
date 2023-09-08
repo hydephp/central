52134ab4a65783851cefecf2aba898bdac71864f
Caen De Silva <caen@desilva.se>
2023-09-08 17:24:27 +0200

    User implements FilamentUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -58,4 +60,9 @@ class User extends Authenticatable
         }
         return $user;
     }
+
+     public function canAccessPanel(Panel $panel): bool
+     {
+         return true;
+     }
 }
