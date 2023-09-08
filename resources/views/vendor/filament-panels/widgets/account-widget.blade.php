f512b4a12cc5ce4fdf1aa86c774475cdfc35af48
Caen De Silva <caen@desilva.se>
2023-09-08 22:04:40 +0200

    Add text to the widget

--- resources/views/vendor/filament-panels/widgets/account-widget.blade.php
+++ resources/views/vendor/filament-panels/widgets/account-widget.blade.php
@@ -12,8 +12,11 @@
                         class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
                 >
                     {{ __('filament-panels::widgets/account-widget.welcome', ['app' => config('app.name')]) }},
-                    {{ filament()->getUserName($user) }}
+                    {{ filament()->getUserName($user) }}!
                 </h2>
+                <p class="text-sm text-gray-500 dark:text-gray-400 pe-8">
+                    This website will give you quick access and insight to all things <x-filament::link href="https://hydephp.com/">HydePHP</x-filament::link>!
+                </p>
             </div>
 
             <form
