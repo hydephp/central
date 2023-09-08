7ff510b5a2655fdea98ae123e2b216f48f239781
Caen De Silva <caen@desilva.se>
2023-09-08 19:37:32 +0200

    Write Git version to disk after update

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@ php artisan down
 git pull
 php ~/composer.phar install --no-dev --no-interaction
 php artisan up
+
+# Write Git version to file as shell_exec is disabled
+git rev-parse HEAD > storage/interop/VERSION
