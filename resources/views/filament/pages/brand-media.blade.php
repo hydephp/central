12404d076855e8a9f24af2c93ab7e0294a97d53a
Caen De Silva <caen@desilva.se>
2023-09-09 14:28:35 +0200

    Revert "Add compact grid option"
    This reverts commit 95ba4813a9a2bb15b14e86ec82bd708f0dc1f2f0.

--- resources/views/filament/pages/brand-media.blade.php
+++ resources/views/filament/pages/brand-media.blade.php
@@ -40,11 +40,7 @@
     </x-filament::card>
 
     <x-filament::card>
-        <section @class([
-            'grid',
-            'grid-cols-7 gap-2' => $compact,
-            'grid-cols-3 gap-4' => ! $compact,
-        ])>
+        <section class="grid grid-cols-3 gap-4">
             @foreach($this->getItems() as $item)
                 <figure class="border rounded-xl flex flex-col">
                     <img src="{{ $item->download }}" alt="{{ $item->name }}" class="p-4 cursor-pointer" loading="lazy" onclick="window.open('{{ $item->link }}')" title="View on GitHub">
