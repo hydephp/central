97f681ec57a2fd4fb8a8a145dfdd3ece1eea01c0
Caen De Silva <caen@desilva.se>
2023-09-09 10:47:14 +0200

    Restructure extracted class

--- app/AppPanel.php
+++ app/AppPanel.php
@@ -2,83 +2,36 @@
 
 namespace App;
 
-use App\Http\Middleware\GuestableAuthenticate;
 use App\Utils\HeroIcon;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Navigation\NavigationBuilder;
 use Filament\Navigation\NavigationGroup;
 use Filament\Navigation\NavigationItem;
-use Filament\Support\Colors\Color;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Filament\Panel;
-use Filament\Widgets;
 use Filament\Pages;
 
 class AppPanel
 {
-    public static function make(Panel $panel): Panel
+    public static function navigation(NavigationBuilder $builder): NavigationBuilder
     {
-        return $panel
-            ->default()
-            ->id('app')
-            ->path('')
-            ->login()
-            ->colors([
-                'primary' => Color::Indigo,
-            ])
-            ->favicon(url('favicon.ico'))
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
-            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
-                return $builder->groups([
-                    NavigationGroup::make('Quick Navigation')->items([
-                        NavigationItem::make('Dashboard')
-                            ->icon('heroicon-o-home')
-                            ->isActiveWhen(fn(): bool => request()->routeIs('filament.admin.pages.dashboard') || request()->url() === url(''))
-                            ->url(fn(): string => Pages\Dashboard::getUrl()),
-                    ]),
-                    NavigationGroup::make('HydePHP Services')->items([
-                        static::externalLinkItem('Main Website', 'https://hydephp.com', 'home'),
-                        static::externalLinkItem('Documentation', 'https://hydephp.com/docs/1.x', 'book-open'),
-                        static::externalLinkItem('Open Analytics', 'https://analytics.hydephp.com', 'chart-bar-square'),
-                    ]),
-                    NavigationGroup::make('Community Resources')->items([
-                        static::externalLinkItem('Discord Server', 'https://discord.hydephp.com', HeroIcon::ChatBubbleLeftRight),
-                        static::externalLinkItem('Community Portal', 'https://hydephp.com/community.html', HeroIcon::UserGroup),
-                        static::externalLinkItem('Developer Resources', 'https://hydephp.com/community.html#developers', HeroIcon::CommandLine),
-                        static::externalLinkItem('GitHub Organisation', 'https://github.com/hydephp', HeroIcon::CodeBracketSquare),
-                        static::externalLinkItem('Source Monorepo', 'https://github.com/hydephp/develop', HeroIcon::BuildingLibrary),
-                    ]),
-                ]);
-            })
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                GuestableAuthenticate::class,
-            ]);
+        return $builder->groups([
+            NavigationGroup::make('Quick Navigation')->items([
+                NavigationItem::make('Dashboard')
+                    ->icon('heroicon-o-home')
+                    ->isActiveWhen(fn(): bool => request()->routeIs('filament.admin.pages.dashboard') || request()->url() === url(''))
+                    ->url(fn(): string => Pages\Dashboard::getUrl()),
+            ]),
+            NavigationGroup::make('HydePHP Services')->items([
+                static::externalLinkItem('Main Website', 'https://hydephp.com', 'home'),
+                static::externalLinkItem('Documentation', 'https://hydephp.com/docs/1.x', 'book-open'),
+                static::externalLinkItem('Open Analytics', 'https://analytics.hydephp.com', 'chart-bar-square'),
+            ]),
+            NavigationGroup::make('Community Resources')->items([
+                static::externalLinkItem('Discord Server', 'https://discord.hydephp.com', HeroIcon::ChatBubbleLeftRight),
+                static::externalLinkItem('Community Portal', 'https://hydephp.com/community.html', HeroIcon::UserGroup),
+                static::externalLinkItem('Developer Resources', 'https://hydephp.com/community.html#developers', HeroIcon::CommandLine),
+                static::externalLinkItem('GitHub Organisation', 'https://github.com/hydephp', HeroIcon::CodeBracketSquare),
+                static::externalLinkItem('Source Monorepo', 'https://github.com/hydephp/develop', HeroIcon::BuildingLibrary),
+            ]),
+        ]);
     }
 
     protected static function externalLinkItem(string $label, string $url, null|string|HeroIcon $icon = null): NavigationItem
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,12 +4,9 @@ namespace App\Providers\Filament;
 
 use App\AppPanel;
 use App\Http\Middleware\GuestableAuthenticate;
-use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Navigation\NavigationBuilder;
-use Filament\Navigation\NavigationGroup;
-use Filament\Navigation\NavigationItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -22,13 +19,45 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use App\Utils\HeroIcon;
 
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-       return AppPanel::make($panel);
+        return $panel
+            ->default()
+            ->id('app')
+            ->path('')
+            ->login()
+            ->colors([
+                'primary' => Color::Indigo,
+            ])
+            ->favicon(url('favicon.ico'))
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return AppPanel::navigation($builder);
+            })
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                GuestableAuthenticate::class,
+            ]);
     }
-
 }
