2e975ef850dbdeec697258114ad5f99bc8fdc524
Caen De Silva <caen@desilva.se>
2023-09-09 14:10:08 +0200

    Create bytesToHuman macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Views\FilamentRenderView;
 use App\Views\Footer;
 use Filament\Support\Facades\FilamentView;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,15 @@ class AppServiceProvider extends ServiceProvider
     {
         FilamentView::registerRenderHook('panels::body.end', Footer::make());
         FilamentView::registerRenderHook('panels::auth.login.form.after', FilamentRenderView::anonymous('components.login-as-guest-button'));
+
+        Str::macro('bytesToHuman', /** Format the given number of bytes into a human-readable format. */
+            function (int $bytes, $precision = 2): string {
+                for ($i = 0; $bytes > 1024; $i++) {
+                    $bytes /= 1024;
+                }
+
+                return round($bytes, $precision) . ' ' . ['B', 'KB', 'MB', 'GB', 'TB'][$i];
+            }
+        );
     }
 }
