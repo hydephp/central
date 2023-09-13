4eda7a6cb0a02a712b3b156104034242c40cd27d
Caen De Silva <caen@desilva.se>
2023-09-13 22:00:50 +0200

    Tweak grid more

--- resources/views/filament/widgets/account-widget.blade.php
+++ resources/views/filament/widgets/account-widget.blade.php
@@ -13,13 +13,13 @@
                 >
                     {{ __('Welcome to :app, :user!', ['app' => config('app.name'), 'user' => filament()->getUserName($user)]) }}
                 </h2>
-                <div class="text-sm text-gray-500 dark:text-gray-400 grid md:grid-cols-5">
-                    <p class="mb-4 col-span-2">
+                <div class="text-sm text-gray-500 dark:text-gray-400 grid md:grid-cols-12">
+                    <p class="mb-4 col-span-5">
                         This website will give you quick access and insight to all things
                         <x-filament::link href="https://hydephp.com/">HydePHP</x-filament::link>
                         &ndash; the static site generator you've been waiting for.
                     </p>
-                    <p class="col-span-3">
+                    <p class="col-span-7 md:ps-6">
                         Please note that this platform is intended for those who wish to contribute
                         to the library source code itself, and if you are simply looking to create
                         a site with HydePHP, you may instead want to head over to the
