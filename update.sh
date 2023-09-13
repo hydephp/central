ee0402e335ab902fc1cba0267945286bdbb0ca50
Caen De Silva <caen@desilva.se>
2023-09-13 19:13:29 +0200

    Add system specific post update hook

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+exit;
 if [ -e "storage/interop/UPDATE.lock" ]; then
     echo "Fatal: An update is already in progress";
     exit 1;
@@ -11,6 +12,11 @@ php artisan up
 
 # Write Git version to file as shell_exec is disabled
 git rev-parse HEAD > storage/interop/VERSION
+
+# A hook to run system specific post-deployment code
+if [ -e ".post-update.sh" ]; then
+    bash .post-update.sh;
+fi
 
 rm storage/interop/UPDATE.lock;
 exit 0;
