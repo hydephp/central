a19e861d5b74d34abf9fa7468633b84227c8e13f
Caen De Silva <caen@desilva.se>
2023-09-13 13:27:54 +0200

    Add experiment banner

--- resources/views/filament/pages/experiments/github-post-creator.blade.php
+++ resources/views/filament/pages/experiments/github-post-creator.blade.php
@@ -1,3 +1,5 @@
 <x-filament-panels::page>
-
+    <div class="bg-emerald-300 p-4 rounded-lg">
+        This is an experiment, and may be unstable and is subject to change and/or be removed. Please send feedback!
+    </div>
 </x-filament-panels::page>
