a288626a0e1b0e9fa2014e6037a869aaddab3494
Caen De Silva <caen@desilva.se>
2023-09-13 18:18:45 +0200

    Move footer component and improve positioning

--- resources/views/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="mt-auto py-2 px-4 text-center w-screen sm:relative mx-auto" style="left: -320px">
-    <small class="opacity-70">
-        {{ config('app.name') }} <small>v</small><code class="font-mono">{{ $footer->version() }}</code>
-    </small>
-    <small class="opacity-50">|</small>
-    <small class="opacity-70 text-primary-500 hover:underline">
-        <a href="https://status.hydephp.com/" target="_blank" rel="nofollow noopener noreferrer">Global Status Page</a>
-    </small>
-</footer>
