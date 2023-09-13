f95062d61532918136ff9914a33ff9040b59202e
Caen De Silva <caen@desilva.se>
2023-09-13 13:31:33 +0200

    Create info card

--- resources/views/filament/pages/experiments/github-post-creator.blade.php
+++ resources/views/filament/pages/experiments/github-post-creator.blade.php
@@ -2,4 +2,14 @@
     <div class="bg-emerald-300 p-4 rounded-lg">
         This is an experiment, and may be unstable and is subject to change and/or be removed. Please send feedback!
     </div>
+
+    <x-filament::card class="prose max-w-full ">
+        <x-filament::section.heading>
+            Welcome to the experimental GitHub blog post creator for HydePHP sites!
+        </x-filament::section.heading>
+
+        <p class="mt-2">
+            This form allows you to visually create blog posts for HydePHP projects with the source hosted on GitHub.
+        </p>
+    </x-filament::card>
 </x-filament-panels::page>
