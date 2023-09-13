bc1744c78fc7c3d29ab64138cc417ee49e664b42
Caen De Silva <caen@desilva.se>
2023-09-13 18:20:01 +0200

    Update footer text

--- resources/views/filament/components/footer.blade.php
+++ resources/views/filament/components/footer.blade.php
@@ -5,7 +5,7 @@
         </small>
         <small class="opacity-50">|</small>
         <small class="opacity-70 text-primary-500 hover:underline">
-            <a href="https://status.hydephp.com/" target="_blank" rel="nofollow noopener noreferrer">Global Status Page</a>
+            <a href="https://status.hydephp.com/" target="_blank" rel="nofollow noopener noreferrer">Global Status Monitor</a>
         </small>
     </div>
 </footer>
